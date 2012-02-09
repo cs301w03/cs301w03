7bdd5a7ef1ccfa2be2874fc9d2835794912c0311
hcngo <hcngo@ualberta.ca>
2012-02-09 16:22:18 -0700

    Hieu Update

--- src/android/hieu/Project2Activity.java
+++ src/android/hieu/Project2Activity.java
@@ -12,6 +12,6 @@ public class Project2Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         System.out.println("Is there anybody out there?\n");
-        System.out.println("Not if you can hear me!\n");
+        System.out.println("Not if you can hear meHieu NGO!\n");
     }
 }

e53b7b37256ea4fcee627b06add13789290f41f9
tsajed <tsajed@ualberta.ca>
2012-03-14 22:48:32 -0600

    new viewlistactivity and xml layouts for list views

--- src/uml/pro2/ViewListActivity.java
+++ src/uml/pro2/ViewListActivity.java
@@ -1,12 +1,28 @@
 package uml.pro2;
 
+
 import android.app.Activity;
+import android.os.Bundle;
 
 
-public class ViewListActivity extends Activity implements FView {
+public class ViewListActivity extends ListActivity {
 
+	
+	
 	@Override
-	public void update(M model) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+        
+	}
+        
+        
+	@Override
+	public void update(M model)
+	{
+
+		// TODO Auto-generated method stub
+		
 	}
 
 }

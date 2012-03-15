93e56b7f1950181997a89e7e09288294d2d9db5d
hcngo <hcngo@ualberta.ca>
2012-03-15 09:59:54 -0600

    IMPORTANT: You all can start right now with the DbControllerInterface... ISSUE: 1/ Tri said the Prof wants us to use Data Obj instead of the raw data type from the database when showing views, meaning we need to avoid using Cursor...However, I don't see the advantage of using data Obj because we can just use the data obtained from the database directly. Why should we bother with Data Obj??? Edit and Delete operations may make use of data obj ... 2/ You guys need to google about how to bind data to view of the listview and how to get the ID of the item chosen so that we can call another activity.Obviously,it's ListAdapter but I don't know how we can get the GroupID or SkinConditionID or photoId when we click at some row of the listview. The method onListItemClick has the rowId but it seems to have nothing to do with the Id of the data bound to the view...

--- src/cmput301W12/android/project/DbControllerInterface.java
+++ src/cmput301W12/android/project/DbControllerInterface.java
@@ -1,19 +1,37 @@
 package cmput301W12.android.project;
 
 import java.util.Set;
+import android.database.Cursor;
 
 public interface DbControllerInterface {
 	public void close();
-	public PhotoObj addPhotoObject(PhotoObj phoObj);
+	
+	/**
+	 * Add this newly created photo to the database. Update the association of this photo with
+	 * any group or skin condition.
+	 * @param phoObj
+	 * @return
+	 */
+	public PhotoObj addPhotoObj(PhotoObj phoObj);
+	
+	/**
+	 * Fetch all photos in the database.
+	 * @return
+	 */
+	public Cursor fetchAllPhotoObj();
+	
 	/**
 	 * Fetch all photos of a group or skin condition with the itemId.
+	 * If itemId = DbAdapter.INVALID_ID then fetch all photos of all containers
+	 * of particular type ( GROUP or SKINCONDITION).
 	 * option = OptionType.GROUP to fetch from the group.
 	 * option = OptionType.SKINCONDITION to fetch from the skin condition.
 	 * @param itemId
 	 * @param option
-	 * @return
+	 * @return The Cursor has all the columns of table PHOTO_TABLE: PHOTOID, LOCATION,
+	 * TIMESTAMP and NAME.
 	 */
-	public Set<PhotoObj> fetchAllPhotoObj(int itemId, OptionType option);
+	public Cursor fetchAllPhotoObjConnected(int itemId, OptionType option);
 	
 	/**
 	 * Fetch all containers (groups or skins conditions) of the photo with the provided photoId.
@@ -21,9 +39,35 @@ public interface DbControllerInterface {
 	 * option = OptionType.GROUP for fetching all groups
 	 * option = OptionType.SKINCONDITION for fetching all skin conditions.
 	 * @param option
-	 * @return
+	 * @return the Cursor has two columns : GROUPID and GROUPNAME or SKINCONDITIONID and
+	 * SKINNAME. 
 	 */
-	public Set<? extends ContainObj> fetchAllContainers(int photoId, OptionType option);
+	public Cursor fetchAllContainers(int photoId, OptionType option);
 	
-	public 
+	/**
+	 * 
+	 * @param option OptionType.GROUP or OptionType.SKINCONDITION to create new
+	 * group or skin condition.
+	 * @return the GROUPID or SKINCONDITIONID of the new group or skin
+	 */
+	public int createContainer(OptionType option);
+	
+	// NOTICE: The three methods above are enough for project part 3. Specifically,
+	// TRI: call fetchAllContainers(DbAdapter.INVALID_ID, OptionType.GROUP) to obtain all the groups.
+	// call fetchAllContainers(DbAdapter.INVALID_ID, OptionType.SKINCONDITION) to obtain all the skin conditions.
+	
+	// TANVIR: call fetchAllPhotoObj() to obtain all photos in the database
+	/// call fetchAllPhotoObjConnected(ID of the group or skin condition, OptionType.GROUP or OptionType.SKIN CONDITION)
+	// to obtain all photos associated with some group or skin condition.
+	
+	// KALLEN: call addPhotoObj(phoObj) to add the newly created photo along with its association 
+	// with any group and skin conditions to the database. 
+	
+	// NOTICE you GUYS: we need to have activity to add new group or skin condition. This activity
+	// is called when we click "menu" and choose "create new group" within the activity "show all groups" of TRI.
+	// or when we click "menu" and choose "create new skin condition" within the activity "show all skin conditions" of TRI.
+	// Also it's used within the activity "showing the newly created photo and let user attach it to group or skin conditiion " 
+	// of KALLEN in case the user would like to attach it with new group or skin condition.
+	
+	// LAST BUT NOT LEAST: we postpone "delete" and "edit" for now...
 }

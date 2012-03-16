141a849eb68564e241a27d85bdd410dd25d4e53e
tsajed <tsajed@ualberta.ca>
2012-03-15 20:41:04 -0600

    Tri's updated project

---
+++ src/cmput301W12/android/project/ContainerArrayAdapter.java
@@ -0,0 +1,35 @@
+package cmput301W12.android.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+public class ContainerArrayAdapter extends ArrayAdapter<Group> {
+		private final Context context;
+		private final Container[] containers;
+
+		public ContainerArrayAdapter(Context context, Container[] containers) {
+			super(context, R.layout.list_text_view);
+			this.context = context;
+			this.containers = containers;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View rowView = inflater.inflate(R.layout.list_photo_view, parent, false);
+			TextView textView = (TextView) rowView.findViewById(R.id.textLabel);
+			if ( textView != null)
+				if (containers.length > 0)
+					if (position < containers.length)
+					{
+						textView.setText(containers[position].getName());
+					}
+			return rowView;
+		}
+	}
---
+++ src/cmput301W12/android/project/CreateGroupActivity.java
@@ -0,0 +1,14 @@
+package cmput301W12.android.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateGroupActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+}

fc34b1472bda8d963d6e47984b4944f15b6ae06f
Skroman <romansky@ualberta.ca>
2012-03-15 18:13:43 -0600

    Added my ContextMenu code from assignment one into PhotoListActivity for Tanvir.

--- src/cmput301W12/android/project/PhotoListActivity.java
+++ src/cmput301W12/android/project/PhotoListActivity.java
@@ -4,6 +4,12 @@ import com.log.R;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
 
 
 public class PhotoListActivity extends ListActivity
@@ -13,6 +19,11 @@ public class PhotoListActivity extends ListActivity
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.listphotoview_activity);
+	        
+	        
+	        //set up the listview to enable a context menu for editting logs/deleting
+	        registerForContextMenu(getListView());  // calls oncontextmenucreate with the ListView,
+	                                                // binding the listview, so the onListItemClick function works
 	 }
 	 
 	 public void fillupList(void) {
@@ -23,4 +34,53 @@ public class PhotoListActivity extends ListActivity
 	 //of options like "add photo", or whatever is given in the UI,
 	 //we are not creating a context menu of list photo, because 
 	 //the list is being handled by the listactivity here.
+
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	    // places the data in ctxmenu into the build the context menu for Petrol manager
+	    @Override
+	    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	        super.onCreateContextMenu(menu,  v, menuInfo);
+	        MenuInflater inflater = getMenuInflater();
+	        inflater.inflate(R.menu.ctxmenu, menu);
+
+	    }	 
+	 
+	 
+	 
+	 // if a user picks an item from the context menu, this function is then called.
+	    protected void onListItemClick(ListView l, View v, int position, long id) {
+	                super.onListItemClick(l, v, position, id);
+	                xid = id;
+	                this.openContextMenu(l);                                  // opens up the context menu
+	        }
+	        
+	    //Opens the xontext menu and gives the option edit, or delete, and reacts accordingly.
+	    public boolean onContextItemSelected(MenuItem item){
+//	              String edit = "Edit Entry";
+//	              String delete = "Delete Entry";
+
+	                //ADD HERE what you would like to do when a list item is selected.
+	                
+//	              if (edit.compareTo(item.getTitle().toString()) == 0){
+//	                      //will have to use a bundle to pass the rowid to Entry_Editor to enable simpler code
+//	                      Intent editor = new Intent(this, Entry_Editor.class);
+//	                      editor.putExtra("_id", Long.toString(xid));
+//	                      startActivityForResult(editor, EDIT_ENTRY);
+//	              }
+//	              else if(delete.compareTo(item.getTitle().toString()) == 0) {
+//	                      mydb.deleteEntry(xid);
+//	                      refreshList();
+//	              }
+	                return super.onContextItemSelected(item);
+	        }
+
+
+
+
 }

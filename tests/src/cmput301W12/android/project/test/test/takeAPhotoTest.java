e04148b3f9b46d708640ccf6dbc6405f666dddf0
Skroman <romansky@ualberta.ca>
2012-03-16 05:08:45 -0600

    I attempted to set this up again.

---
+++ tests/src/cmput301W12/android/project/test/test/takeAPhotoTest.java
@@ -0,0 +1,21 @@
+package cmput301W12.android.project.test.test;
+
+import junit.framework.TestCase;
+
+
+public class takeAPhotoTest extends TestCase
+{
+
+    public void testOnCreateBundle()
+    {
+
+        fail("Not yet implemented");
+    }
+
+    public void testOnActivityResultIntIntIntent()
+    {
+
+        fail("Not yet implemented");
+    }
+
+}

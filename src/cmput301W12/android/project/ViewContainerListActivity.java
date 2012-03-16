8f76c84231032a4cbc4caff8d667bfd68c945c38
tsajed <tsajed@ualberta.ca>
2012-03-15 21:52:01 -0600

    Commint Tri's ViewContainerListActivity

---
+++ src/cmput301W12/android/project/ViewContainerListActivity.java
@@ -0,0 +1,83 @@
+package cmput301W12.android.project;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ViewContainerListActivity extends ListActivity implements FView<DbController>{
+	private static final int ACTIVITY_CREATE=0;
+	private static final int CREATE_ID = Menu.FIRST;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_text_view);
+        
+        fillData(new Container[]{new Group(1, "test")});
+        
+        registerForContextMenu(getListView());
+    }
+    
+    private void fillData(Container[] con) {
+        
+        Group g1 = new Group(1,"Head");
+        Group g2 = new Group(2,"Left hand");
+        Group g3 = new Group(3,"Right Feet");
+        
+        Group[] gl = {g1,g2,g3};
+        
+        ContainerArrayAdapter conAdapter = new ContainerArrayAdapter(this, gl);        
+        setListAdapter(conAdapter);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        ListAdapter adapter = l.getAdapter();
+        Container cont  = (Container) adapter.getItem(position);
+        Intent i = new Intent(this, CreateGroupActivity.class);
+        i.putExtra("cid", cont);
+        startActivity(i);
+    }    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, CREATE_ID, 0, "Add New");
+        return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+            case CREATE_ID:
+            	createNewGroup();
+                return true;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+    
+    /**
+     * Pass an intent to EntryAdd with request code ACTIVITY_CREATE
+     * to create a new log entry and wait for the result.
+     */
+    private void createNewGroup() {
+        Intent i = new Intent(this, CreateGroupActivity.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if ( requestCode == ACTIVITY_CREATE)
+        	fillData(new Container[]{new Group(1, "test")});
+    }
+    
+}

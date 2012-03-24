57e0c3e95b295761601758de58462793c0e012f7
hcngo <hcngo@ualberta.ca>
2012-03-23 18:16:49 -0600

    testing after fixing local master.

---
+++ src/cmput301W12/android/project/FController.java
@@ -0,0 +1,32 @@
+package cmput301W12.android.project;
+
+import java.util.Set;
+import java.util.SortedSet;
+
+/**
+ * @author Hieu Ngo
+ * @date Mar 14, 2012
+ * 
+ * Interface for Controller of the system in MVC model.
+ * Every controller should implement this interface
+ */
+public interface FController {
+
+	public void close();
+
+	public Photo addPhoto(Photo photo);
+
+	public Container addContainObj(Container container);
+
+	public SortedSet<Photo> getAllPhoto();
+
+	public SortedSet<Photo> getAllPhotoOfAContainer(int itemId, OptionType option);
+
+	public Set<? extends Container> getAllContainersOfAPhoto(int photoId, OptionType option);
+
+	public Set<? extends Container> getAllContainers(OptionType option);
+	
+	public int deleteEntry(long rowID, OptionType option);
+
+
+}

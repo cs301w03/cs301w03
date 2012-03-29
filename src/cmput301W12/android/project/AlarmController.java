97b040ce305328e529e93ff3de0d6f037cf1be9b
Firstname Lastname <tsajed@ualberta.ca>
2012-03-28 22:49:53 -0600

    LJJO

--- src/cmput301W12/android/project/AlarmController.java
+++ src/cmput301W12/android/project/AlarmController.java
@@ -1,5 +1,7 @@
 package cmput301W12.android.project;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Calendar;
 
 
@@ -7,23 +9,58 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.os.SystemClock;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
 public class AlarmController extends Activity
 {
+	private static final String MEDIA_PLAYER = null;
 	Toast mToast;
-
+	MediaPlayer mp;
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.alarm_controller);
+        
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.alarm_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        
+        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
+        mp = new MediaPlayer();
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE); 
+        try {
+			mp.setDataSource(this, alert);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         // Watch for button clicks.
         Button button = (Button)findViewById(R.id.one_shot);
@@ -117,5 +154,83 @@ public class AlarmController extends Activity
             mToast.show();
         }
     };
+    
+    public class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent,
+            View view, int pos, long id) {
+          Toast.makeText(parent.getContext(), "The planet is " +
+              parent.getItemAtPosition(pos).toString() + pos, Toast.LENGTH_LONG).show();
+          
+          if (pos == 0) {
+        	  setonetimeAlarm();
+          }
+          
+          else {
+        	  setrepeatingAlarm();
+          }
+        }
+
+        public void onNothingSelected(AdapterView parent) {
+          // Do nothing.
+        }
+    }
+
+	public void setonetimeAlarm() {
+		// TODO Auto-generated method stub
+		
+		Intent intent = new Intent(this, AlarmReceiver.class);
+		Bundle bundle = new Bundle();
+		//bundle.putParcelable(MEDIA_PLAYER, (Parcelable) mp);
+		//intent.putExtras(bundle);
+        PendingIntent sender = PendingIntent.getBroadcast(this,
+                0, intent, 0);
+
+        // We want the alarm to go off 30 seconds from now.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 30);
+
+        // Schedule the alarm!
+        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+
+        // Tell the user about what we did.
+        if (mToast != null) {
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(AlarmController.this, "Testing One Shot",
+                Toast.LENGTH_LONG);
+        mToast.show();
+		
+	}
+
+	public void setrepeatingAlarm() {
+		// TODO Auto-generated method stub
+		
+		Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(this,
+                0, intent, 0);
+
+        // We want the alarm to go off 30 seconds from now.
+        long firstTime = SystemClock.elapsedRealtime();
+        firstTime += 15*1000;
+
+        // Schedule the alarm!
+        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        firstTime, 15*1000, sender);
+
+        // Tell the user about what we did.
+        if (mToast != null) {
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(this, "Testing Repeating Shots",
+                Toast.LENGTH_LONG);
+        mToast.show();
+		
+	}
+    
+    
 
 }

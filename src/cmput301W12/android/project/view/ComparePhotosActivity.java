9700b8820f1a6466585cddfa44e7c9e41a93228d
Skroman <romansky@ualberta.ca>
2012-03-29 15:48:38 -0600

    untracked bin, added new layout's, new activity, ammended some stuff in editphoto to make more stuff on the context menu

---
+++ src/cmput301W12/android/project/view/ComparePhotosActivity.java
@@ -0,0 +1,26 @@
+package cmput301W12.android.project.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cmput301W12.android.project.R;
+
+
+public class ComparePhotosActivity extends Activity
+{
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compare_photos);
+        
+        /* setup list adapter to use the photo list */
+        
+    }
+    
+}

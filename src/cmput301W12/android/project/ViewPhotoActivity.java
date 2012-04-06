666b795486abb6b299bd3fdedde905c2e3b4eeaf
romansky <romansky@ualberta.ca>
2012-04-05 21:51:05 -0600

    started working on show annotation in the ViewPhotoActivity.

--- src/cmput301W12/android/project/ViewPhotoActivity.java
+++ src/cmput301W12/android/project/ViewPhotoActivity.java
@@ -21,10 +21,16 @@ import java.io.OutputStream;
 import java.sql.Timestamp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -36,6 +42,9 @@ public class ViewPhotoActivity extends Activity implements FView<DbController>
 	private TextView timestamp;
 	private TextView skincondition;
 	private TextView group;
+	private Photo photo = null;
+	
+	public static final int ID_CREATE_ANNOTATION = Menu.FIRST;
 
 	private static void saveBMP( File intentPicture, Bitmap ourBMP) throws IOException, FileNotFoundException {
 		OutputStream out = new FileOutputStream(intentPicture);
@@ -48,7 +57,8 @@ public class ViewPhotoActivity extends Activity implements FView<DbController>
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.view_photo);
-
+		
+		photo = (Photo) getIntent().getSerializableExtra("PHOTO");
 		newimage = (ImageView) findViewById(R.id.imageView1);
 		timestamp = (TextView) findViewById(R.id.viewphototext1);
 		skincondition = (TextView) findViewById(R.id.viewphototext2);
@@ -62,7 +72,7 @@ public class ViewPhotoActivity extends Activity implements FView<DbController>
 
 	public void getPhotos(){
 		try{
-			Photo photo = (Photo) getIntent().getSerializableExtra(PhotoListActivity.PHOTO);
+			//photo = (Photo)getIntent().getSerializableExtra(PhotoListActivity.PHOTO);
 			String location = photo.getLocation();
 			Uri uri = Uri.parse(location);
 			Bitmap bitmap = BitmapFactory.decodeFile(uri.getPath());
@@ -76,8 +86,56 @@ public class ViewPhotoActivity extends Activity implements FView<DbController>
 
 	}
 
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, ID_CREATE_ANNOTATION, 0, "View Photo Annotation");
+        return true;
+    }
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()) {
+		case ID_CREATE_ANNOTATION:
+			showAnnotation(photo, this);
+			//return true;
+		}
+
+            return super.onMenuItemSelected(featureId, item);
+	}
+	
+	
+	public void showAnnotation(final Photo photo, Context context) {
+		AlertDialog.Builder popupBuilder = new AlertDialog.Builder(context);
+		popupBuilder.setTitle("Annotation");
+		final EditText annotation = new EditText(context);
+		//final Photo photo = (Photo)getIntent().getSerializableExtra(PhotoListActivity.PHOTO);
+		if (photo.getAnnotation().equals(""))
+		{
+			annotation.setSingleLine();
+			annotation.setText("");
+		}
+		else
+		{
+			annotation.setSingleLine();
+			annotation.setText(photo.getAnnotation());
+		}
+		popupBuilder.setView(annotation);
+		popupBuilder.setNeutralButton("Confirm", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//When click confirm save the annotation into the photo?
+				photo.setAnnotation(annotation.getText().toString());
+				
+			}
+		});
+		popupBuilder.create();
+		popupBuilder.show();
+	}
+	
 	public void getTimeStamp() {
-		Photo photo = (Photo) getIntent().getSerializableExtra(PhotoListActivity.PHOTO);
+		//Photo photo = (Photo) getIntent().getSerializableExtra(PhotoListActivity.PHOTO);
 		Timestamp ts = photo.getTimeStamp();
 		String time = ts.toString();
 		timestamp.setText(time);
